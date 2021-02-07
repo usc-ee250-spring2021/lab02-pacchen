ad0698f4a50de172859a05b9eb7048e5a23b43a9
Paul <chenpaul@usc.edu>
2021-02-06 16:00:59 -0800

    1

--- ee250/lab02/grove_rotary_angle_sensor.py
+++ ee250/lab02/grove_rotary_angle_sensor.py
@@ -149,8 +149,8 @@ while True:
         # Give PWM output to LED
         grovepi.analogWrite(led,brightness)
 
-        print("sensor_value = %d voltage = %.2f degrees = %.1f brightness = %d" %(sensor_value, voltage, degrees, brightness))
-        except KeyboardInterrupt:
+                print("sensor_value = %d voltage = %.2f degrees = %.1f brightness = %d" %(sensor_value, voltage, degrees, brightness))
+    except KeyboardInterrupt:
         grovepi.analogWrite(led,0)
         break
     except IOError:
