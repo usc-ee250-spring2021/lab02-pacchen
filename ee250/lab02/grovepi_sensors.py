e4d092a0e6a9bae713283ee65710aa5241afdb0e
pacchen <77824060+pacchen@users.noreply.github.com>
2021-02-07 22:45:24 -0800

    Update grovepi_sensors.py

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -1,15 +1,8 @@
 """ EE 250L Lab 02: GrovePi Sensors
 
 List team members here.
-    setText("Shades of red")
-    for c in range(0,255):
-        setRGB(255,255-c,255-c)
-        time.sleep(.01)
-
-           setText("White")
-    setRGB(255,255,255)
-    time.sleep(.5)
 Insert Github repository link here.
+
 """
 
 """python3 interpreters in Ubuntu (and other linux distros) will look in a
@@ -66,26 +59,6 @@ def setRGB(r,g,b):
 def textCommand(cmd):
     bus.write_byte_data(DISPLAY_TEXT_ADDR,0x80,cmd)
 
-# set display text \n for second line(or auto wrap)
-def setText(text):
-    textCommand(0x01) # clear display
-    time.sleep(.05)
-    textCommand(0x08 | 0x04) # display on, no cursor
-    textCommand(0x28) # 2 lines
-    time.sleep(.05)
-    count = 0
-    row = 0
-    for c in text:
-        if c == '\n' or count == 16:
-            count = 0
-            row += 1
-            if row == 2:
-                break
-            textCommand(0xc0)
-            if c == '\n':
-                continue
-        count += 1
-        bus.write_byte_data(DISPLAY_TEXT_ADDR,0x40,ord(c))
 
 #Update the display without erasing the display
 def setText_norefresh(text):
